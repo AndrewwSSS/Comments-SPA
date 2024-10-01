3abfbaec175563084b43e35eb77c83d7c53a6855
AndrewwSSS <clash2clans1one@gmail.com>
2024-10-01 19:48:03 +0300

    create Comment model

--- backend/comments/models.py
+++ backend/comments/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    parent_message = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="replies"
+    )
+    content = models.TextField()
