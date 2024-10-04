38df49be7bf9b63eb5899ade5d5f471f79a7b0b0
AndrewwSS <clash2clans1one@gmail.com>
2024-10-04 17:19:18 +0300

    add cors settings

--- backend/comments_spa/settings.py
+++ backend/comments_spa/settings.py
@@ -20,12 +20,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "user",
     "comments",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -34,6 +36,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080"
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = "comments_spa.urls"
 
