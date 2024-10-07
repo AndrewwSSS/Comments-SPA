2e712e51a9c160255a29051c7bb82a526f1c5209
AndrewwSS <clash2clans1one@gmail.com>
2024-10-07 22:25:11 +0300

    fix http interceptor

--- frontend/src/helpers/axios.interceptor.js
+++ frontend/src/helpers/axios.interceptor.js
@@ -5,34 +5,17 @@ axios.interceptors.request.use(
     config => {
         const user = JSON.parse(localStorage.getItem('user'));
 
+        if (config.url.includes('/api/users/token/refresh/')) {
+            store.dispatch('auth/logout');
+            window.location.href = '/login';
+            return
+        }
+
         if (user && user.access) {
             config.headers['Authorization'] = 'Bearer ' + user.access;
         }
         return config;
     },
-    async error => {
-        if (!error.response || error.response.status !== 401) {
-            return Promise.reject(error);
-        }
-
-        const originalRequest = error.config;
-
-        if (originalRequest.url.includes('/api/users/token/refresh/')) {
-            await store.dispatch('auth/logout');
-            window.location.href = '/login';
-            return
-        }
-        //
-        // let user = JSON.parse(localStorage.getItem('user'));
-        //
-        // if (!user) {
-        //     await store.dispatch('auth/logout');
-        //     window.location.href = '/login';
-        //     return Promise.reject(error);
-        // }
-
-        return Promise.reject(error);
-    }
 );
 
 
@@ -53,24 +36,24 @@ axios.interceptors.response.use(
         return Promise.reject(error);
       }
 
-      let refresh_token = user.refresh;
-      try {
-        const response = await axios.post(
-            `http://127.0.0.1:8000/api/users/token/refresh/`,
-            { refresh: refresh_token }
-        );
+    let refresh_token = user.refresh;
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:8000/api/users/token/refresh/`,
+        { refresh: refresh_token }
+      );
 
         if (response.status === 200) {
           user.access = response.data.access;
           localStorage.setItem('user', JSON.stringify(user));
 
-          error.config.headers['Authorization'] = 'Bearer ' + response.data.access;
-          return axios(error.config);
-        }
-      } catch (refreshError) {
-        await store.dispatch('auth/logout');
-        window.location.href = '/login';
+        error.config.headers['Authorization'] = 'Bearer ' + response.data.access;
+        return axios(error.config);
       }
+    } catch (refreshError) {
+      await store.dispatch('auth/logout');
+      window.location.href = '/login';
+    }
 
       return Promise.reject(error);
     }
